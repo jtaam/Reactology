7f1837561b5a094c143029ae5314de45ee1553e1
Tam Jid <tamjid.mdh@gmail.com>
2018-05-29 17:14:59 +0600

    hardcoded props

--- src/Project.js
+++ src/Project.js
@@ -8,11 +8,11 @@ class Project extends Component {
           <div className="row">
             <div className="col-md-7">
               <a href="#">
-                <img className="img-fluid rounded mb-3 mb-md-0" src="https://res.cloudinary.com/jtam/image/upload/v1527549848/universe-2742113_1920_vuzmhp.jpg" alt="" />
+                <img className="img-fluid rounded mb-3 mb-md-0" src={this.props.image} alt="" />
               </a>
             </div>
             <div className="col-md-5">
-              <h3>Project One</h3>
+              <h3>{this.props.title}</h3>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium veniam exercitationem expedita laborum at voluptate. Labore, voluptates totam at aut nemo deserunt rem magni pariatur quos perspiciatis atque eveniet unde.</p>
               <a className="btn btn-primary" href="#">View Project</a>
             </div>
--- src/index.js
+++ src/index.js
@@ -16,10 +16,10 @@ ReactDOM.render(
   <App />
   <PageHeading />
   {/* <Contents /> */}
-  <Project />
-  <Project />
-  <Project />
-  <Project />
+    <Project image="https://res.cloudinary.com/jtam/image/upload/v1527549848/universe-2742113_1920_vuzmhp.jpg" title="IC 1101" />
+    <Project image="https://res.cloudinary.com/jtam/image/upload/v1527549851/milky-way-2695569_1920_sijzuz.jpg" title="Andromeda" />
+    <Project image="https://res.cloudinary.com/jtam/image/upload/v1527549848/milky-way-923738_1920_sqoxje.jpg" title="Milky Way" />
+    <Project image="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3c/The_Orion_Nebula_M42.jpg/1280px-The_Orion_Nebula_M42.jpg" title="Orion" />
   <Pagination />
   <Footer />
   </div>,

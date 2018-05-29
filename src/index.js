a4d6971225e0421909edea97eb6cf97327ceb476
Tam Jid <tamjid.mdh@gmail.com>
2018-05-29 15:53:42 +0600

    pagination

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import Footer from './Footer';
 import PageHeading from './PageHeading';
 import Project from './Project';
 // import Contents from './Contents';
+import Pagination from './Pagination';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(
@@ -19,6 +20,7 @@ ReactDOM.render(
   <Project />
   <Project />
   <Project />
+  <Pagination />
   <Footer />
   </div>,
   document.getElementById('root'));

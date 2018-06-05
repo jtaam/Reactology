badd63de9e5d7062a85535a13f66c6f8ebb0da34
_tam <tamjid84@yahoo.com>
2018-06-05 22:56:50 +0600

    about page

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -2,13 +2,13 @@ import React from 'react';
 
 const Footer = () => {
   return (
-    <div>
-      <footer className="py-5 bg-dark">
-        <div className="container">
-          <p className="m-0 text-center text-white">Copyright © Reactology 2018</p>
-        </div>
-      </footer>
-    </div>
+
+    <footer class="py-5 bg-dark">
+      <div class="container">
+        <p class="m-0 text-center text-white">Copyright © Reactology 2018</p>
+      </div>
+    </footer>
+
   )
 }
 export default Footer;
